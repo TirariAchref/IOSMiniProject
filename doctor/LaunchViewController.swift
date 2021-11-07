7a06b02737612d641e668f387e4e3de80dc4cb34
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2021-11-07 15:26:26 -0800

    interface

---
+++ doctor/LaunchViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LaunchViewController.swift
+//  doctor
+//
+//  Created by achref on 7/11/2021.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindtoLaunch1(segue:
+                                   UIStoryboardSegue){
+        
+        print("back")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ doctor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  doctor
+//
+//  Created by Med Wajdi BRAHEM on 7/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
